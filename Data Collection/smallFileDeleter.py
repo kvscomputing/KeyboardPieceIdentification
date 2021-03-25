b83c971db9ecba656999e5953cf89d9f76ad95c3
kberdos <71733278+kberdos@users.noreply.github.com>
2021-03-25 12:34:03 -0400

    Update smallFileDeleter.py

--- Data Collection/smallFileDeleter.py
+++ Data Collection/smallFileDeleter.py
@@ -3,10 +3,11 @@ import csv
 
 path = input("Enter file path for your splits: ")
 arr_txt = [x for x in os.listdir(path) if x.endswith(".csv")]
+limit = int(input("Enter the row limit: "))
 for x in arr_txt:
     file = open(path  + '\\' +str(x))
     reader = csv.reader(file)
     lines = sum(1 for row in reader)
     file.close()
-    if(lines < int(input("Enter the row limit: "))):
+    if(lines < limit):
         os.remove(x)
